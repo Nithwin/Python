652322b7b2d67553e81a0672acce1c8481476017
Nithwin <vmnithwin@gmail.com>
2025-12-22 21:42:50 +0530

    Add initial implementation of data structures and string methods examples

---
+++ main.py
@@ -0,0 +1,176 @@
+# Set
+# s = {1,2,3,4}
+# s = {i**2 for i in s}
+# print(s)
+# s = {1,2,3,6,2,4}
+# #l = [1,2,3,4]
+# s2 = set()
+# s2.add(5)
+# s2.add(4)
+# s2.add(3)
+# s2.add(3)
+# s2.update([9,4,19,34])
+# #s2.remove(100)
+# #s2.discard(100)
+# s2.pop()
+# #s2.clear()
+# s2 = s2.difference({9,4})
+# subset = {7,6,5,4,3,2}
+# print({99,6}.issubset(subset))
+# print(s2)
+
+
+
+
+
+# Tuple
+#immutable 
+# tp = (1,2,3,4)
+# tp2 = (9,8,7)
+# #tp[0] = 2
+
+# print(tp.index(4))
+# print(len(tp))
+# print(max(tp))
+# print(min(tp))
+# print(sum(tp))
+# print(tp * 3)
+
+# x,y,z = tp2
+# print(x,y,z)
+
+# # List 
+# ls = [1,2,3,4]
+# ls = [i**2 for i in range(0,len(ls)) if i%2 == 0]
+# # for i in range(0,len(ls)):
+# #     ls[i] = ls[i] * ls[i]
+# print(ls)
+
+
+
+# ls.append(5)
+# ls.extend([86,4,3])
+# ls.insert(1, 1000)
+# ls.remove(1000)
+# ls.pop()
+# ls.clear()
+# print(ls.index("Hi"))
+# print(ls.count(3))
+# ls.sort()
+# ls.reverse()
+# ls2 = ls.copy()
+# ls2[0] = 100000
+# print(ls)
+# print(ls2)
+# print(len(ls))
+# print(max(ls))
+# print(min(ls))
+# print(sum(ls))
+#print(dir(ls))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# String Methods
+# s = "Python is an programming language"
+# S = 'Another'
+# a = "1233"
+# print(s.upper())
+# print(s.lower())
+# print(s.title())
+# print(s.capitalize())
+# print(s.swapcase())
+# print(s.isupper())
+# print(s.islower())
+# print(s.isalpha())
+# print(a.isdigit())
+# print(s.find('is'))
+# print(s.count('l'))
+# print(s.startswith('Python'))
+# print(s.endswith('end'))
+# print(s.replace('Python', 'C++'))
+# print(s.rstrip())
+# print(s.lstrip())
+# ls = s.split(' ')
+# print(" ".join(ls))
+# print("{} {} Good".format('College','is'))
+# name = "Laptop"
+# print(f"{name} is Good")
+# print(len(name))
+# print(dir(s))
+#print("College" + " " + "is")
+# Loops
+
+
+# for i in range(11, 0, -1):
+#     if i%2 == 0:
+#         break
+#     else:
+#         print(i,end=' ')
+
+# i = 0
+# while i <= 10:
+#     print(i, end=' ')
+#     i += 1
+
+
+
+#Control Statements
+"""
+num1 = 1
+num2 = 2
+if num1 == num2:
+    print(num1+num2)
+elif num1 == 1 and num2 == 2:
+    print(num1*num2)
+else:
+    print(num2-num1)
+"""
+
+
+
+# a = 4
+# b = 5
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a ** b)
+# print(a / b)
+# print(a // b)
+
+# a *= b
+# print(a)
+
+# b1 = True
+# b2 = False
+
+# print(b1 and b2)
+# print(b1 or b2)
+# print(not b2)
+
+# print(b1 & b2)
+# print(b1 | b2)
+
+# a = 'Hello'
+# num = 34547645637.56
+# flag = False
+
+# print(type(num))
+# a = input('Enter your name: ')
+"""
+print(a)
+print(a)
+print(a)
+print(a)
+print(a)
+"""
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+#s2.remove(100)
+s2.discard(100)
+s

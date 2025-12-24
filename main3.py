36bdd91c159a312d94af63baf13780ab5a739227
Nithwin <vmnithwin@gmail.com>
2025-12-24 10:31:30 +0530

    Added main3.py

---
+++ main3.py
@@ -0,0 +1,15 @@
+# Class and Objects
+
+
+
+
+
+
+#print([age for i in range(0, int(input())) if (age:=int(input())) >= 18])
+
+# n = int(input())
+# c = 0
+# for i in range(0, n):
+#     if (age := int(input())) >= 18:
+#             c += age
+# print(c)
